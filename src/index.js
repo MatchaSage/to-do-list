fc2051fa2132714e5d97648a99590061c1e11c1c
Treyce Wheeler <t.zeryon@gmail.com>
2022-08-28 13:50:55 -0400

    Add forms and styling for forms

--- src/index.js
+++ src/index.js
@@ -3,12 +3,30 @@ import displayCategories from "./displayCategories";
 import createCategory from "./createCategory";
 
 let newCategoryButton = document.querySelector('.new.category.button');
-
+let newTodoButton = document.querySelector('.new.todo');
 let defaultCategory = createCategory();
 displayCategories(defaultCategory);
 
+let cancelCategory = document.querySelector('.cancel.category');
+let categoryForm = document.getElementById('newCategory');
+
+newTodoButton.addEventListener('click', function() {
+    
+})
+
+cancelCategory.addEventListener('click', function(event) {
+    event.preventDefault();
+    categoryForm.style.visibility = 'hidden';
+})
+
 newCategoryButton.addEventListener('click', function() {
-    let newCategory = prompt('New Category', '')
+    if (categoryForm.style.visibility == 'hidden') {
+        categoryForm.style.visibility = 'visible';
+    }
+    else {
+        categoryForm.style.visibility = 'hidden';
+    }
+
     let createdCategory = createCategory(newCategory);
     displayCategories(createdCategory);
 })
